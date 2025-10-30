5e15a4d2cd0c434acceffb9afcfd664647f96c8e
aymeric <totofrancois12@gmail.com>
2025-10-30 11:12:54 +0100

    add brelan

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-export const test = (diceThrower) => {
-  return diceThrower.flat().reduce((sum, dice) => sum + dice, 0);
-}
+export const yamsScore = (diceThrower) => {
+  const counts = {};
+  diceThrower.flat().forEach(d => counts[d] = (counts[d] || 0) + 1);
+  const hasThreeOfAKind = Object.values(counts).some(c => c >= 3);
+  if (hasThreeOfAKind) {
+    return 28
+  }else{
+    return diceThrower.flat().reduce((sum, dice) => sum + dice, 0);
+  }
+} 
--- index.test.js
+++ index.test.js
@@ -1,8 +1,13 @@
 import { describe, it, expect } from "vitest";
-import { test } from "./index.js";
+import { yamsScore } from "./index.js";
 
 describe("Sample Test", () => {
   it("should return give [[1,1,1,5,6]] pass", () => {
-    expect(test([[1,1,1,5,6]])).toBe(14);
+    expect(yamsScore([[1,1,1,5,6]])).toBe(14);
+  });
+});
+describe("Test Brelan", () => {
+  it("should return give [[1,2,3,4,6],[1,2,3,4,6]] pass", () => {
+    expect(yamsScore([[1,2,3,4,6],[1,2,3,4,6]])).toBe(32);
   });
 });
